efd89ba5c7edf39101328f7844bf3c42ec3ad7b1
Pavithra Kodmad <pkodmad@atlassian.com>
2021-04-07 19:54:50 +1000

    Get a convo

--- api/redirect.js
+++ api/redirect.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const FormData = require("form-data");
+const { WebClient, LogLevel } = require("@slack/web-api");
 
 module.exports = async (req, res) => {
   const { query } = req;
@@ -34,7 +35,23 @@ module.exports = async (req, res) => {
       }
     );
     console.log("successful response!");
-    res.json({ success: true, payload: payload.data });
+    const token = payload.data.access_token;
+    const client = new WebClient(token, {
+      // LogLevel can be imported and used to make debugging simpler
+      logLevel: LogLevel.DEBUG,
+    });
+    // https://eggheadio.slack.com/archives/C030CS0RL/p1613379294013000
+    const conversation = await app.client.conversations.history({
+      // The token you used to initialize your app
+      token,
+      channel: "C030CS0RL",
+      // In a more realistic app, you may store ts data in a db
+      latest: "p1613379294013000",
+      // Limit results
+      inclusive: true,
+      limit: 1,
+    });
+    res.json({ success: true, payload: conversation });
   } catch (err) {
     console.error(err);
     res.json({ success: false, payload: null, error: err });
